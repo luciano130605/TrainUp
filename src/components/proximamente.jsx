9b9424459ca8eb4ca6236e89e8dccc73c25fc29b
luciano <luciano.delapena@gmail.com>
2026-08-13 16:22:15 -0300

    fix design y proximamente

--- src/components/proximamente.jsx
+++ src/components/proximamente.jsx
@@ -50,6 +50,7 @@ export default function ProximamentePage() {
                         </div>
                     ))}
                 </div>
+                <br ></br>
             </div>
         </>
     );
